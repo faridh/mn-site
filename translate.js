46d5d63a935ae396038909c3a446221003b648e4
Faridh Mendoza <faridh@intangible.mx>
2016-06-14 10:32:25 -0500

    First Commit

---
+++ translate.js
@@ -0,0 +1,126 @@
+/*HOME*/
+
+window.locale = {
+    "menu": {
+      "home": [
+        "Home",
+        "Inicio",
+        "Iniciação"
+      ]
+
+      "about" :[
+        "About us",
+        "Nosotros",
+        "Sobre nós"
+      ]
+
+      "portfolio" : [
+        "Portfolio",
+        "Portafolio",
+        "Portólio"
+      ]
+
+      "team" : [
+        "Team",
+        "Equipo",
+        "Equipe"
+      ]
+
+      "contact" : [
+        "Contact",
+        "Contacto",
+        "Contato"
+      ]
+
+
+      "home": {
+        "intro": [
+          "By Entrepreneurs <br>for entrepreneurs</br>",
+          "De emprendedores <br>para emprendedores</br>",
+          "De empreendedores <br>para empreendedores</br>"
+        ]
+
+        "subtitle": [
+          "We believe Latin America is ready to take an important role in the global entrepreneurial ecosystem.",
+          "Creemos que América Latina esta lista para tomar un rol importante en el ecosistema de empreendedorismo global.",
+          "Acreditamos que a América Latina está pronta para assumir um importante papel no ecossistema de empreendedorismo global."
+        ]
+
+        "paragraph": [
+          "We believe latin america is ready to take an important role in the global entrepreneurial ecosystem. the market, its founders, the innovative potential and regional high impact startups are starting to come together. we want nazca to be the driving force and become a major player in this phenomena.",
+          "Creemos que América Latina esta lista para tomar un rol importante en el ecosistema de empreendedorismo global. El Mercado, Sus Fundadores, el potencial de innovación y Startup regionales de alto impacto comienzan a encontrarse. Queremos que Nazca sea la fuerza motora y se torne un miembro relevante de este fenomeno.";
+          "Acreditamos que a América Latina está pronta para assumir um importante papel no ecossistema de empreendedorismo global. O Mercado, Seus Fundadores, o Potencial de Inovação e as Startups Regionais de Alto Impacto começam a se encontrar. Queremos que Nazca seja a força motriz e se torne um membro relevante deste fenômeno."
+        ]
+
+        "more_button": [
+          "Learn more",
+          "Ver más",
+          "Saiba mais"
+        ]
+
+        "apply_button": [
+          "Introduce your company",
+          "Presente su empresa",
+          "Apresente sua empresa"
+        ]
+
+      },
+      "footer": {
+        "contact" [
+          "Contact",
+          "Contacto",
+          "Contato"
+        ]
+      }
+
+/*ABOUT*/
+    "about": {
+      "what": [
+        "What",
+        "Qué",
+        "O qué"
+      ]
+
+      "what_description" : [
+        "Nazca is a regional Venture Capital Fund by entrepreneurs for entrepreneurs. We offer a multi-stage funding estructure: from idea to product, product to growth and growth to profitability.",
+        "Nazca es un Fondo Regional de Capital de Riesgo hecho por emprendedores para emprendedores. Ofrecemos estructuras de financiamiento multi-nivel: de la idea al produto, del producto al crecimiento y del crecimiento al lucro.",
+        "Nazca é um Fundo Regional de Capital de Risco feito por empreendedores para empreendendores. Nós oferecemos uma estrutura multi-nível de financiamento: da idéia ao produto, do produto para o crescimento e  do crescimento para a lucratividade."
+        ]
+
+
+      "where" : [
+        "Where",
+        "Dónde",
+        "Onde"
+      ]
+
+      "where_description" : [
+        "Latin-America. We hold offices in Argentina, Brazil, Chile, Colombia and México and continue to grow our regional presence.",
+        "América Latina. Tenemos oficinas en Argentina, Brasil, Chile, Colombia y México y continuamos creciendo nuestra presencia regional.",
+        "América Latina. Nós temos escritórios na Argentina, Brasil, Chile, Colômbia e México e continuamos crescendo nossa presença regional."
+      ]
+
+
+      "why" : [
+        "Why",
+        "Por qué",
+        ""
+      ]
+
+      "how" : [
+      "how",
+      "cómo",
+      "como"
+    ]
+
+    "how_description" : [
+      "By creating a regional Venture Capital Fund that unifies Latin America, Nazca will facilitate the regional roll-out of local companies and maximize value through consolidation. We will strategically promote entrepreneurs and their companies who understand Latin America as a whole and help them execute their vision.",
+      "Creando un Fondo Regional de Venture Capital que unifique América Latina, Nazca facilitará la expansión regional (roll-out)  de compañías locales maximizando el valor mediante la consolidación. Promoveremos estratégicamente a empreendedores y sus compañías que entiendan América Latina como un todo y le ayudaremos a ejecutar su visión.",
+      "Com a criação de um Fundo de Venture Capital regional que unifica a América Latina, Nazca facilitará o lançamento regional de empresas locais e maximizará valor através da consolidação. Promoveremos de forma estratégica os empreendedores e suas empresas que entendem a América Latina como um todo, e os ajudaremos a executar suas visões."
+
+  ]
+
+
+
+
+    };
